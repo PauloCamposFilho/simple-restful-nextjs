67ac393ea3d99e26725bc68f4e304229a61c92f0
Paulo Campos Filho <pezaum@gmail.com>
2023-11-27 09:43:14 -0800

    Add simple stylesheet

--- pages/RESTCountries.js
+++ pages/RESTCountries.js
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import styles from '../styles/Home.module.css';
+import RESTCountriesStyles from '../styles/RESTCountries.module.css';
+import useCountryData from "../hooks/RESTCountries/useCountryData";
 
 export default function RESTCountries() {
+  const {
+    inputValue,
+    responseData,
+    handleInputChange,
+    resetInputValue,
+    getResponseData,
+    handleResponseData
+  } = useCountryData();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,6 +21,11 @@ export default function RESTCountries() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className={styles.title}>RESTCountries</h1>
+      {inputValue != '' &&
+        <p>Debug: {inputValue}</p>
+      }
+      <input type="text" placeholder="Enter Country Name" value={inputValue} onChange={handleInputChange} />
+      <button className={RESTCountriesStyles.search} type="button">Get info!</button>
     </div>
   );
 }
