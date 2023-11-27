cd89cc745e2112831af8675b8d0212561eb770b3
Paulo Campos Filho <pezaum@gmail.com>
2023-11-27 09:51:57 -0800

    Remove debugging statements

--- helpers/fetchCountryData.js
+++ helpers/fetchCountryData.js
@@ -3,14 +3,9 @@ import axios from '../utils/axios';
 const fetchCountryData = async (countryName) => {
   console.log("We're in the fetchCountryData.js");
   try {
-    console.log("Trying to get info...");
     const responseData = await axios.get(`${countryName}`);
-    alert(responseData.data[0].name.common);
-    console.log("Got it!");
     return responseData.data;
   } catch (error) {
-    console.log("Something is up:")
-    console.log(error);
     return { status: "error", message: error };
   }
 };
