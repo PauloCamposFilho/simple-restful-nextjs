eac0d806d7dba452691033a9e1d956d5761d19e8
Paulo Campos Filho <pezaum@gmail.com>
2023-11-27 19:28:06 -0800

    Add basic compact listing

--- components/CountryItem.js
+++ components/CountryItem.js
@@ -5,19 +5,12 @@ const CountryItem = (props) => {
     countryNames.push(`${language}: ${props.name.nativeName[language].common}`);
   }
   return (
-    <div className={styles.country}>
-      <div className={styles.flag_container}>
-        <img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.flags.alt}`} />
-      </div>
-      <h2 className={styles.name}>{props.name.common} - {props.cca3}</h2>
-      <h3 className={styles.official_name}>Official Name: {props.name.official}</h3>
-      <div className={styles.stats}>
-        <span className={styles.stats}>Population: {props.population.toLocaleString()}</span>
-        <span className={styles.stats}>Indepent: {props.independent ? "Yes" : "No"}</span>
-        <span className={styles.stats}>Region: {props.region}</span>
-        <span className={styles.stats}>Sub-region: {props.subregion}</span>
-      </div>
-    </div>
+    <tr onClick={() => { alert(`${props.name.common}`) }}>
+      <td><img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.name.alt}`} /></td>
+      <td>{props.name.common}</td>
+      <td>{props.cca3}</td>
+      <td>{props.population.toLocaleString()}</td>
+    </tr>
   );
 };
 
--- components/CountryList.js
+++ components/CountryList.js
@@ -4,11 +4,21 @@ import RESTCountriesStyles from '../styles/CountryList.module.css';
 const CountryList = ({ countries }) => {
   return (
     <div className={RESTCountriesStyles.container_wrapper}>
-      <div className={RESTCountriesStyles.container}>
-        {countries.map((country) => {
-          return <CountryItem key={country.cca3} {...country} />
-        })}
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>{/* Flag */}</th>
+            <th>Country</th>
+            <th>Code</th>
+            <th>Population</th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => {
+            return <CountryItem key={country.cca3} {...country} />
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
