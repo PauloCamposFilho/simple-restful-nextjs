7e3f5723830e390ae31355f7665e562c27541575
Paulo Campos Filho <pezaum@gmail.com>
2023-11-27 19:04:52 -0800

    Add some more basic styling

--- components/CountryList.js
+++ components/CountryList.js
@@ -1,13 +1,15 @@
 import Country from "./Country";
-import RESTCountriesStyles from '../styles/RESTCountries.module.css';
+import RESTCountriesStyles from '../styles/CountryList.module.css';
 
 const CountryList = ({ countries }) => {
   return (
-  <div className={RESTCountriesStyles.container}>
-    {countries.map((country) => {
-      return <Country key={country.cca3} {...country} />
-    })}
-  </div>
+    <div className={RESTCountriesStyles.container_wrapper}>
+      <div className={RESTCountriesStyles.container}>
+        {countries.map((country) => {
+          return <Country key={country.cca3} {...country} />
+        })}
+      </div>
+    </div>
   );
 };
 
