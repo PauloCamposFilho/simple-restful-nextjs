ced3faa5e91bc326250dbbf5ad5a60c4e779bc3e
Paulo Campos Filho <pezaum@gmail.com>
2023-11-29 10:58:05 -0800

    Return to simplistic table. Add hover effect and max-width to screen

--- components/CountryItem.js
+++ components/CountryItem.js
@@ -1,27 +1,20 @@
-import { ButtonBase, Card, CardActionArea, CardContent, Typography, ListItem, ListItemAvatar, ListItemText, Avatar, TableRow, TableCell } from '@mui/material';
 import { useDialog } from '../contexts/DialogContext';
 import styles from '../styles/CountryItem.module.css';
 import Country from './Country';
-
+import { TableRow, TableCell } from '@mui/material';
 const CountryItem = (props) => {
   const { showDialog } = useDialog();
 
   const handleCountryClick = () => {
-    showDialog(props.name.official, Country(props));
-  }
+    showDialog(props.name.official, Country(props))
+  };
 
   return (
-    <TableRow hover onClick={handleCountryClick}>
-      <TableCell>
-        <img
-          src={props.flags.svg}
-          alt={`${props.name.common} flag`}
-          style={{ width: '30px', height: 'auto' }}
-        />
-      </TableCell>
-      <TableCell>{props.name.common}</TableCell>
-      <TableCell>{props.cca3}</TableCell>
-      <TableCell>{props.population.toLocaleString()}</TableCell>
+    <TableRow onClick={handleCountryClick} sx={{ '&:hover': { backgroundColor: 'rgba(255, 255, 255, 0.1)' } }}>
+      <TableCell><img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.name.alt}`} /></TableCell>
+      <TableCell sx={{ color: 'white' }}>{props.name.common}</TableCell>
+      <TableCell sx={{ color: 'white' }}>{props.cca3}</TableCell>
+      <TableCell sx={{ color: 'white' }}>{props.population.toLocaleString()}</TableCell>
     </TableRow>
   );
 };
--- components/CountryList.js
+++ components/CountryList.js
@@ -1,35 +1,27 @@
 import CountryItem from "./CountryItem";
 import RESTCountriesStyles from '../styles/CountryList.module.css';
-import { Grid, Card, CardContent, List, ListItem, ListItemAvatar, ListItemText, Avatar, Paper, TableBody, Table } from "@mui/material";
-import { FixedSizeList } from 'react-window';
-import Country from "./Country";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 
 const CountryList = ({ countries }) => {
-  const countryRow = ({ index, style }) => {
-    const country = countries[index];
-    return <CountryItem key={country.cca3} style={style} {...country} />;
-  };
   return (
-    <Paper className={RESTCountriesStyles.container_wrapper}>
-      {/* <Table>
+    <div className={RESTCountriesStyles.container_wrapper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>Country</TableCell>
+            <TableCell>Code</TableCell>
+            <TableCell>Population</TableCell>
+          </TableRow>
+        </TableHead>
         <TableBody>
           {countries.map((country) => {
             return <CountryItem key={country.cca3} {...country} />
           })}
         </TableBody>
-      </Table> */}
-      <FixedSizeList
-        height={400}
-        width='100%'
-        itemSize={50}
-        itemCount={countries.length}
-        overscanCount={50}
-      >
-        {countryRow}
-      </FixedSizeList>
-
-    </Paper>
+      </Table>
+    </div>
   );
-}
+};
 
 export default CountryList;
