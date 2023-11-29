682df9ff93803b2bab569ee656d9d97a5373629c
Paulo Campos Filho <pezaum@gmail.com>
2023-11-29 13:44:55 -0800

    Replace <img> for <Image> component

--- components/Country.js
+++ components/Country.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from '../styles/Country.module.css';
 const Country = (props) => {
   const languages = Object.values(props.languages).join(', ');
@@ -8,8 +9,15 @@ const Country = (props) => {
   return (
     <div className={styles.country}>
       <div className={styles.flag_container}>
-        <img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.flags.alt}`} />
-        <img className={styles.flag} src={props.coatOfArms.svg} alt={`${props.name.common} coat of arms.`} />
+        <Image
+          className={styles.flag}
+          src={props.flags.svg}
+          alt={`${props.name.common} flag. ${props.flags.alt}`}
+        />
+        <Image
+          className={styles.flag}
+          src={props.coatOfArms.svg}
+          alt={`${props.name.common} coat of arms.`} />
       </div>
       <h2 className={styles.name}>{props.name.common} - {props.cca3}</h2>
       <h3 className={styles.official_name}>{props.name.official}</h3>
@@ -25,10 +33,10 @@ const Country = (props) => {
         <span className={styles.stats}>Currencies: {currencies}</span>
       </div>
       <div className={styles.map_app_container}>
-        <a className={styles.map_app_info} href={props.maps.openStreetMaps} target='_blank'><img className={styles.map_app_icon} src='/osm.svg' /><span>View in OpenStreetMaps</span></a>
+        <a className={styles.map_app_info} href={props.maps.openStreetMaps} target='_blank'><Image className={styles.map_app_icon} alt='OpenStreetMaps icon'  src='/osm.svg' /><span>View in OpenStreetMaps</span></a>
       </div>
       <div className={styles.map_app_container}>
-        <a className={styles.map_app_info} href={props.maps.googleMaps} target='_blank'><img className={styles.map_app_icon} src='/gmaps.svg' /><span>View in GoogleMaps</span></a>
+        <a className={styles.map_app_info} href={props.maps.googleMaps} target='_blank'><Image className={styles.map_app_icon} alt='GoogleMaps icon' src='/gmaps.svg' /><span>View in GoogleMaps</span></a>
       </div>
     </div>
   );
