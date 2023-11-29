75077a41bf42f90445862525848e42533f31ac1d
Paulo Campos Filho <pezaum@gmail.com>
2023-11-29 09:56:05 -0800

    Try Card structure for list/countries

--- components/CountryItem.js
+++ components/CountryItem.js
@@ -1,19 +1,39 @@
+import { ButtonBase, Card, CardActionArea, CardContent, Typography } from '@mui/material';
 import { useDialog } from '../contexts/DialogContext';
 import styles from '../styles/CountryItem.module.css';
 import Country from './Country';
+// const CountryItem = (props) => {
+//   const { showDialog } = useDialog();
+//   const countryNames = [];
+//   for (const language in props.name.nativeName) {
+//     countryNames.push(`${language}: ${props.name.nativeName[language].common}`);
+//   }
+//   return (
+//     <tr onClick={() => { showDialog(props.name.official, Country(props)) }}>
+//       <td><img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.name.alt}`} /></td>
+//       <td>{props.name.common}</td>
+//       <td>{props.cca3}</td>
+//       <td>{props.population.toLocaleString()}</td>
+//     </tr>
+//   );
+// };
+
 const CountryItem = (props) => {
   const { showDialog } = useDialog();
-  const countryNames = [];
-  for (const language in props.name.nativeName) {
-    countryNames.push(`${language}: ${props.name.nativeName[language].common}`);
-  }
+
   return (
-    <tr onClick={() => { showDialog(props.name.official, Country(props)) }}>
-      <td><img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} flag. ${props.name.alt}`} /></td>
-      <td>{props.name.common}</td>
-      <td>{props.cca3}</td>
-      <td>{props.population.toLocaleString()}</td>
-    </tr>
+    <ButtonBase onClick={() => { showDialog(props.name.official, Country(props)) }}>
+      <Card className={styles.card}>
+        <CardActionArea>
+          <img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} falg. ${props.name.alt}`} />
+          <CardContent>
+            <Typography variant='h6' component="div">{props.name.common}</Typography>
+            <Typography variant='body2' color="text.secondary">Code: {props.cca3}</Typography>
+            <Typography variant='body2' color="text.secondary">Population: {props.population.toLocaleString()}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </ButtonBase>
   );
 };
 
--- components/CountryList.js
+++ components/CountryList.js
@@ -1,24 +1,46 @@
 import CountryItem from "./CountryItem";
 import RESTCountriesStyles from '../styles/CountryList.module.css';
+import { Grid, Card, CardContent } from "@mui/material";
+import Country from "./Country";
+
+// const CountryList = ({ countries }) => {
+//   return (
+//     <div className={RESTCountriesStyles.container_wrapper}>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>{/* Flag */}</th>
+//             <th>Country</th>
+//             <th>Code</th>
+//             <th>Population</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {countries.map((country) => {
+//             return <CountryItem key={country.cca3} {...country} />
+//           })}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
 
 const CountryList = ({ countries }) => {
   return (
     <div className={RESTCountriesStyles.container_wrapper}>
-      <table>
-        <thead>
-          <tr>
-            <th>{/* Flag */}</th>
-            <th>Country</th>
-            <th>Code</th>
-            <th>Population</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countries.map((country) => {
-            return <CountryItem key={country.cca3} {...country} />
-          })}
-        </tbody>
-      </table>
+      <Grid container spacing={3}>
+        {countries.map((country) => {
+          return (
+            <Grid item key={country.cca3} xs={12} sm={6} md={4} lg={3}>
+              <Card>
+                <CardContent>
+                  <CountryItem {...country} />
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 };
