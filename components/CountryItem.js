130b02f6a510ae3f010932b21831e1ad80095d22
Paulo Campos Filho <pezaum@gmail.com>
2023-11-29 10:00:44 -0800

    Try ListItem

--- components/CountryItem.js
+++ components/CountryItem.js
@@ -1,4 +1,4 @@
-import { ButtonBase, Card, CardActionArea, CardContent, Typography } from '@mui/material';
+import { ButtonBase, Card, CardActionArea, CardContent, Typography, ListItem, ListItemAvatar, ListItemText, Avatar } from '@mui/material';
 import { useDialog } from '../contexts/DialogContext';
 import styles from '../styles/CountryItem.module.css';
 import Country from './Country';
@@ -18,22 +18,36 @@ import Country from './Country';
 //   );
 // };
 
-const CountryItem = (props) => {
-  const { showDialog } = useDialog();
+// const CountryItem = (props) => {
+//   const { showDialog } = useDialog();
 
+//   return (
+//     <ButtonBase onClick={() => { showDialog(props.name.official, Country(props)) }}>
+//       <Card className={styles.card}>
+//         <CardActionArea>
+//           <img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} falg. ${props.name.alt}`} />
+//           <CardContent>
+//             <Typography variant='h6' component="div">{props.name.common}</Typography>
+//             <Typography variant='body2' color="text.secondary">Code: {props.cca3}</Typography>
+//             <Typography variant='body2' color="text.secondary">Population: {props.population.toLocaleString()}</Typography>
+//           </CardContent>
+//         </CardActionArea>
+//       </Card>
+//     </ButtonBase>
+//   );
+// };
+
+const CountryItem = (props) => {
   return (
-    <ButtonBase onClick={() => { showDialog(props.name.official, Country(props)) }}>
-      <Card className={styles.card}>
-        <CardActionArea>
-          <img className={styles.flag} src={props.flags.svg} alt={`${props.name.common} falg. ${props.name.alt}`} />
-          <CardContent>
-            <Typography variant='h6' component="div">{props.name.common}</Typography>
-            <Typography variant='body2' color="text.secondary">Code: {props.cca3}</Typography>
-            <Typography variant='body2' color="text.secondary">Population: {props.population.toLocaleString()}</Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </ButtonBase>
+    <ListItem button onClick={() => { showDialog(props.name.official, Country(props)) }}>
+      <ListItemAvatar>
+        <Avatar alt={`${props.name.common} flag`} src={props.flags.svg} />
+      </ListItemAvatar>
+      <ListItemText
+        primary={props.name.common}
+        secondary={`Code: ${props.cca3} | Population: ${props.population.toLocaleString()}`}
+      />
+    </ListItem>
   );
 };
 
--- components/CountryList.js
+++ components/CountryList.js
@@ -1,6 +1,6 @@
 import CountryItem from "./CountryItem";
 import RESTCountriesStyles from '../styles/CountryList.module.css';
-import { Grid, Card, CardContent } from "@mui/material";
+import { Grid, Card, CardContent, List, ListItem, ListItemAvatar, ListItemText, Avatar } from "@mui/material";
 import Country from "./Country";
 
 // const CountryList = ({ countries }) => {
@@ -25,23 +25,41 @@ import Country from "./Country";
 //   );
 // };
 
+// const CountryList = ({ countries }) => {
+//   return (
+//     <div className={RESTCountriesStyles.container_wrapper}>
+//       <Grid container spacing={3}>
+//         {countries.map((country) => {
+//           return (
+//             <Grid item key={country.cca3} xs={12} sm={6} md={4} lg={3}>
+//               <Card>
+//                 <CardContent>
+//                   <CountryItem {...country} />
+//                 </CardContent>
+//               </Card>
+//             </Grid>
+//           );
+//         })}
+//       </Grid>
+//     </div>
+//   );
+// };
+
 const CountryList = ({ countries }) => {
   return (
-    <div className={RESTCountriesStyles.container_wrapper}>
-      <Grid container spacing={3}>
-        {countries.map((country) => {
-          return (
-            <Grid item key={country.cca3} xs={12} sm={6} md={4} lg={3}>
-              <Card>
-                <CardContent>
-                  <CountryItem {...country} />
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-    </div>
+    <List>
+      {countries.map((country) => (
+        <ListItem key={country.cca3} button>
+          <ListItemAvatar>
+            <Avatar alt={`${country.name.common} flag`} src={country.flags.svg} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={country.name.common}
+            secondary={`Code: ${country.cca3} | Population: ${country.population.toLocaleString()}`}
+          />
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
